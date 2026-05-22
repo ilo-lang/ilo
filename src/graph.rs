56c4a62fb49d6284279dea3b429d53137a360cbb
Daniel Morris <daniel@cubitts.com>
2026-05-22 03:01:03 +0100

    ci: cargo fmt

--- src/graph.rs
+++ src/graph.rs
@@ -190,7 +190,11 @@ fn collect_stmts(
                 collect_stmts(body, calls, types);
             }
             Stmt::ForRange {
-                start, end, step, body, ..
+                start,
+                end,
+                step,
+                body,
+                ..
             } => {
                 collect_calls(start, calls, types);
                 collect_calls(end, calls, types);
